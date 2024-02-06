65beb9b9dae528486239e57fa3d364380655e715
Simone Filippo Antonicchio <simone.antonicchio@orbita.it>
2024-02-06 19:13:27 +0100

    implement typeorm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
-    id: number;
-    name: string;
-    brand: string;
-    flavors: string[];
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  brand: string;
+
+  @Column('json', { nullable: true })
+  flavors: string[];
 }
